f4c30c3c9611f0c80f9827d280cbcfc633745781
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-27 10:05:09 +0000

    🫦

--- WO/vt2/src/comps/hea.jsx
+++ WO/vt2/src/comps/hea.jsx
@@ -1,16 +1,13 @@
 // heaer component in react
 
 export default function Header() {
-  const ParaText = `
-    This is a header component in React.
-    It spans multiple lines.
-    You can use it for detailed descriptions.
-  `
+  const ParaText =
+    "This is a header component in React.\n It spans multiple lines. \n You can use it for detailed descriptions.  "
 
   return (
     <>
       <h1> Headers </h1>
-      <h3> {ParaText} </h3>
+      <p style={{ whiteSpace: "pre-line" }}>{ParaText}</p>
     </>
   )
 }
