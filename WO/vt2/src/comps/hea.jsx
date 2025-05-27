8981cc35538914816ed111dcf2f5adfce4faccdb
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-27 10:03:27 +0000

    🫦

--- WO/vt2/src/comps/hea.jsx
+++ WO/vt2/src/comps/hea.jsx
@@ -1,12 +1,16 @@
 // heaer component in react
 
 export default function Header() {
-  const ParaText = "This is a header component in React"
+  const ParaText = `
+    This is a header component in React.
+    It spans multiple lines.
+    You can use it for detailed descriptions.
+  `
 
   return (
     <>
       <h1> Headers </h1>
-      <h3> This is coming from the components folder</h3>
+      <h3> {ParaText} </h3>
     </>
   )
 }
