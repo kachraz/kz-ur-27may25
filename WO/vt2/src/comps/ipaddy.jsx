d4508a0917dfc6fa7630be10f0d1a192721ccf16
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-27 11:39:53 +0000

    🫦

--- WO/vt2/src/comps/ipaddy.jsx
+++ WO/vt2/src/comps/ipaddy.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react"
 
-export function useIpAddress() {
+export default function useIpAddress() {
   const [ipAddress, setIpAddress] = useState("")
 
   useEffect(() => {
