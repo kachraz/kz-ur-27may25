1949d4641cf817bf1ae1296a5b3cd1e89c92b135
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-28 23:46:38 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <className='antialiased flex items-center justify-center min-h-screen flex-col'>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </className=>
   )
 }
 
