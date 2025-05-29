f8325670ce1120bb3ec72755a3d5d9fdd65c8933
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:35:45 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -1,11 +1,17 @@
 import { CardDemo } from "./components/ui/cardform"
+import { CalendarDemo } from "./components/ui/mycalendar"
 
 function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
-        <CardDemo></CardDemo>
+        <CardDemo />
+        <CalendarDemo />
+        <p className="text-gray-600 mt-4">
+          This is a simple example of a Vite application using Tailwind CSS and
+          custom components.
+        </p>
       </div>
     </>
   )
