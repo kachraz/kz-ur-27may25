e737fa8f65a0312695c140b712a1c6401b294cd0
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:01:52 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
+        <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
         <Button>Click me</Button>
       </div>
     </>
