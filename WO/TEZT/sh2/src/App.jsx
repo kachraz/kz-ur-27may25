23324835b82593afb78f01c6ba992d7713f58910
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:39:08 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -1,4 +1,5 @@
 import { CardDemo } from "./components/ui/cardform"
+import { CalendarDemo } from "./components/ui/mycalendar"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
         <CardDemo></CardDemo>
+        <CalendarDemo></CalendarDemo>
       </div>
     </>
   )
