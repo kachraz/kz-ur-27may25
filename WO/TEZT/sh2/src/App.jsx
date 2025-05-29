79fba15227169fd4516bb0d5c32ef74006065cf2
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:13:43 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button"
-
 function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
-        <Button>Click me</Button>
       </div>
     </>
   )
