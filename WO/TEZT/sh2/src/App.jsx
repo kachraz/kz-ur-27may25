e569ca143c54f1a336f3a639b6be4a59e199e91e
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:50:46 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
-        <div className="grid grid-cols-2 gap-x-8 gap-y-1">
+        <div className="grid grid-cols-2 gap-x-8 gap-y-1 mt-16">
           <CardDemo />
           <CalendarDemo />
         </div>
