9f49d0f2378063413d028bca9179a4b087ebd8ac
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 00:32:22 +0000

    🫦

--- WO/TEZT/sh2/src/App.jsx
+++ WO/TEZT/sh2/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite UI</h1>
+        <CardDemo></CardDemo>
       </div>
-      <CardDemo></CardDemo>
     </>
   )
 }
